2f81b24a313186b7ac5f1bf4e97ec7b64adad680
Umigishi-Aoi <umigishi.aoi@gmail.com>
2022-04-20 18:52:42 +0900

    set no show debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyWidget(),
     );
   }
